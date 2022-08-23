5535a08e49438b1415c0bba527b5dece7962e8ce
justinpaulosolo <justinpaulosolo@gmail.com>
2022-08-23 09:07:40 -0700

    started on search page/query

---
+++ src/pages/search/[query].ts
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Search(props: any) {
+    console.log(props.anime);
+
+};
+
+export const getServerSideProps = async () => {
+    const res = await fetch("https://api.jikan.moe/v4/anime?letter=sword")
+    const data = res.json();
+
+    return {
+        props: {
+            anime: data
+        }
+    }
+}; 
