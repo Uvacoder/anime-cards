52fe823e2f2de7604be37c7accbe32f5902bee23
justinpaulosolo <justinpaulosolo@gmail.com>
2022-08-23 19:11:00 -0700

    fix3

--- src/pages/search/[query].ts
+++ src/pages/search/[query].ts
@@ -1,8 +1,3 @@
-export default function Search(props: any) {
-    console.log(props.anime);
-
-};
-
 export const getServerSideProps = async () => {
     const res = await fetch("https://api.jikan.moe/v4/anime?letter=sword");
     const data = res.json();
